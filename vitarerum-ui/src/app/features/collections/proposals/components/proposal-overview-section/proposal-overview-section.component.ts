3be9385f7052d7d2107a624b209a4792aef57dd1
Marcos Viana Duchinski <mvid@tjpr.jus.br>
2026-07-24 13:51:28 +0100

    chore: update proposal overview section component

--- vitarerum-ui/src/app/features/collections/proposals/components/proposal-overview-section/proposal-overview-section.component.ts
+++ vitarerum-ui/src/app/features/collections/proposals/components/proposal-overview-section/proposal-overview-section.component.ts
@@ -1,14 +1,5 @@
 import { ChangeDetectionStrategy, Component, input } from '@angular/core';
 
-import {
-  StatusChipComponent,
-  WorkflowStatus,
-} from '@shared/components/status-chip/status-chip.component';
-import {
-  getUseTypePresentation,
-  TypeChipComponent,
-} from '@shared/components/type-chip/type-chip.component';
-
 import { ProposalDetail } from '../../models/proposal.model';
 import {
   formatProposalDetailDateTime,
@@ -19,7 +10,6 @@ import {
   selector: 'app-proposal-overview-section',
   standalone: true,
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [StatusChipComponent, TypeChipComponent],
   templateUrl: './proposal-overview-section.component.html',
   styleUrl: './proposal-overview-section.component.scss',
 })
@@ -27,14 +17,6 @@ export class ProposalOverviewSectionComponent {
   readonly proposal = input.required<ProposalDetail>();
 
   protected readonly formatDateTime = formatProposalDetailDateTime;
-
-  protected asWorkflowStatus(value: string): WorkflowStatus {
-    return value as WorkflowStatus;
-  }
-
-  protected typeLabel(): string {
-    return getUseTypePresentation(this.proposal().type).label;
-  }
 
   protected requesterGroupLabel(): string {
     return PROPOSAL_DETAIL_GROUP_LABELS[this.proposal().requestedBy.group];
